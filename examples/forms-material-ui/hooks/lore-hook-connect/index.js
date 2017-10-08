3a219eb965ebcccf0e591354afaec0a32a7d413e
Jason Hansen <jason_hansen@outlook.com>
2017-10-07 20:10:47 -0700

    [examples/forms-material-ui] expose Connect, connect and getState

--- examples/forms-material-ui/hooks/lore-hook-connect/index.js
+++ examples/forms-material-ui/hooks/lore-hook-connect/index.js
@@ -1,12 +1,21 @@
 /* eslint no-param-reassign: "off" */
 
 import connect from './connect';
+import _connect from './_connect';
+import _getState from './_getState';
+import __Connect from './__Connect';
 import find from './blueprints/find';
 import byId from './blueprints/byId';
 import singleton from './blueprints/singleton';
 import all from './blueprints/all';
 import byCid from './blueprints/byCid';
 import first from './blueprints/first';
+
+export {
+  _connect as connect,
+  _getState as getState,
+  __Connect as Connect
+};
 
 export default {
 
