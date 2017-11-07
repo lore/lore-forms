ace9db16fb5ccfb5f5013b4cd532ef51e0ea1992
Jason Hansen <jason_hansen@outlook.com>
2017-11-06 21:24:35 -0700

    [lore-react-forms-material-ui] export templates

--- packages/lore-react-forms-material-ui/src/index.js
+++ packages/lore-react-forms-material-ui/src/index.js
@@ -21,3 +21,33 @@ export {
   TextAreaField,
   TextField
 };
+
+/**
+ * Templates
+ */
+
+import Action from './templates/Action';
+import Actions from './templates/Actions';
+import Field from './templates/Field';
+import Fields from './templates/Fields';
+import Form from './templates/Form';
+import FormStep from './templates/FormStep';
+import FormSteps from './templates/FormSteps';
+import Request from './templates/Request';
+import Step from './templates/Step';
+import Stepper from './templates/Stepper';
+import SchemaForm from './templates/SchemaForm';
+
+export {
+  Action,
+  Actions,
+  Field,
+  Fields,
+  Form,
+  FormStep,
+  FormSteps,
+  Request,
+  Step,
+  Stepper,
+  SchemaForm
+};
