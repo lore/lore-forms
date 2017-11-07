d17d82051a8deee20a18b12dc123608c5557712e
Jason Hansen <jason_hansen@outlook.com>
2017-11-07 12:10:49 -0700

    [lore-react-forms] add local package

---
+++ examples/forms-material-ui/hooks/lore-react-forms/PropBarrier.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export default React.createClass({
+  displayName: 'PropBarrier',
+
+  propTypes: {
+    element: React.PropTypes.string,
+    className: React.PropTypes.string,
+    style: React.PropTypes.object
+  },
+
+  getDefaultProps: function() {
+    return {
+      element: 'div',
+      className: '',
+      style: {},
+    };
+  },
+
+  render: function() {
+    const {
+      element,
+      className,
+      style,
+      children
+    } = this.props;
+
+    const props = {
+      className: className || null,
+      style: style || {}
+    };
+
+    if (children.length) {
+      return React.createElement(element, props,
+        this.props.children
+      );
+    }
+
+    return React.createElement(element, props,
+      this.props.children
+    );
+
+    return React.cloneElement(this.props.children, props);
+  }
+
+});
