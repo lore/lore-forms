727f84d32e7ed3f67bddfb0bcdbb1c918f8b3f7a
Jason Hansen <jason_hansen@outlook.com>
2017-11-07 12:12:10 -0700

    [webpack] use local form packages

--- examples/forms-material-ui/webpack.config.js
+++ examples/forms-material-ui/webpack.config.js
@@ -54,9 +54,12 @@ module.exports = function(env) {
       alias: {
         'react': path.resolve(__dirname, 'node_modules/react'),
         'material-ui': path.resolve(__dirname, 'node_modules/material-ui'),
-        'lore-hook-forms-material-ui': path.resolve(__dirname, '../../packages/lore-hook-forms-material-ui/es'),
-        'lore-react-forms': path.resolve(__dirname, '../../packages/lore-react-forms/es'),
-        'lore-react-forms-material-ui': path.resolve(__dirname, '../../packages/lore-react-forms-material-ui/es')
+        // 'lore-hook-forms-material-ui': path.resolve(__dirname, '../../packages/lore-hook-forms-material-ui/es'),
+        // 'lore-react-forms': path.resolve(__dirname, '../../packages/lore-react-forms/es'),
+        // 'lore-react-forms-material-ui': path.resolve(__dirname, '../../packages/lore-react-forms-material-ui/es'),
+        'lore-hook-forms-material-ui': path.resolve(__dirname, './hooks/lore-hook-forms-material-ui'),
+        'lore-react-forms': path.resolve(__dirname, './hooks/lore-react-forms'),
+        'lore-react-forms-material-ui': path.resolve(__dirname, './hooks/lore-react-forms-material-ui')
       }
     },
     module: {
