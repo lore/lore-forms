47e3a1cd8587d98056787e0d996940269e8e51d9
Jason Hansen <jason_hansen@outlook.com>
2017-11-24 23:39:53 -0700

    remove dead code

--- examples/forms-material-ui/hooks/lore-hook-connect.partial/errors/InvalidActionKeyError.js
+++
@@ -1,7 +0,0 @@
-export default function InvalidActionKeyError(key) {
-  const error = new Error(
-    `There is no action registered for '${key}'`
-  );
-  error.name = 'InvalidActionKeyError';
-  return error;
-}
