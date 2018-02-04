4dd9b2f039c95733eb149f8a7319028cb3af61b8
Jason Hansen <jason_hansen@outlook.com>
2018-02-04 16:25:01 -0700

    [lore-react-forms] add generic schema based forms

---
+++ packages/lore-react-forms/src/forms/SchemaActions.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import createReactClass from 'create-react-class';
+import { result as _result } from 'lore-utils';
+import FormSection from '../components/FormSection';
+
+export default createReactClass({
+  displayName: 'SchemaActions',
+
+  propTypes: {
+    schema: PropTypes.object.isRequired,
+    actionMap: PropTypes.object.isRequired,
+    actions: PropTypes.array.isRequired,
+    form: PropTypes.object.isRequired,
+  },
+
+  render: function () {
+    const {
+      schema,
+      actionMap,
+      form,
+      actions
+    } = this.props;
+
+    return (
+      <FormSection>
+        {schema.actions(form)(
+          actions.map((action, index) => {
+            const mappedAction = actionMap[action.type];
+            return (
+              React.cloneElement(schema.action(form)(
+                mappedAction(form, _result(action.props, form))
+              ), {
+                key: index
+              })
+            );
+          })
+        )}
+      </FormSection>
+    );
+  }
+
+});
--- packages/lore-react-forms/src/index.js
+++ packages/lore-react-forms/src/index.js
@@ -1,11 +1,11 @@
-import Field from './Field';
-import Form from './Form';
-import FormSection from './FormSection';
-import PropBarrier from './PropBarrier';
+// components
+export { default as Field } from './components/Field';
+export { default as Form } from './components/Form';
+export { default as FormSection } from './components/FormSection';
+export { default as PropBarrier } from './components/PropBarrier';
 
-export {
-  Field,
-  Form,
-  FormSection,
-  PropBarrier
-};
+// forms
+export { default as GenericForm } from './forms/GenericForm';
+export { default as SchemaActions } from './forms/SchemaActions';
+export { default as SchemaFields } from './forms/SchemaFields';
+export { default as SchemaForm } from './forms/SchemaForm';
